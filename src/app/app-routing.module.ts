2a404997b5ac6e3833c6ee215f226d277dfd8c53
Ruslan <gundertajlo260@gmail.com>
2020-10-05 23:57:56 +0300

    u app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllPostComponent } from './post/components/all-post/all-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'post', component: AllPostComponent},
+  {path: 'user', loadChildren: () => import('./user/user.module').then( v => v.UserModule )}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './components/post/post.component';
 import { AllPostComponent } from './components/all-post/all-post.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [PostComponent, AllPostComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
 export class PostModule { }
