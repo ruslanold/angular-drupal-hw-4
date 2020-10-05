581f66ab1057ef043dbf8abf1eb545daa3037510
Ruslan <gundertajlo260@gmail.com>
2020-10-05 23:35:27 +0300

    update post interface

---
+++ src/app/post/interfaces/post.interface.ts
@@ -0,0 +1,17 @@
+export interface IPost{
+    nid: string,
+    vid: string,
+    type: string,
+    language: string,
+    title: string,
+    uid: string,
+    status: string,
+    created: string,
+    changed: string,
+    comment: string,
+    promote: string,
+    sticky: string,
+    tnid: string,
+    translate: string,
+    uri: string 
+}
